31ae3e59739b5523fb834557f0b98d2765da55dd
MeKyleH <kyle.a.holt@gmail.com>
2015-01-13 11:28:44 -0700

    used for creating the database

---
+++ ajaxchat.sql
@@ -0,0 +1,8 @@
+CREATE TABLE chat (
+	message_id int(11) NOT NULL AUTO_INCREMENT,
+	posted_on datetime NOT NULL,
+	user_name varchar(20) NOT NULL,
+	message text NOT NULL,
+	color char(7) default '#000000',
+	PRIMARY KEY (message_id)
+);
